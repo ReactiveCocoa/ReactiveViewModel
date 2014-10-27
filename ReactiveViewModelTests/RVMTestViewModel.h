ebd80b43c04ad51c36ed1985ba24bd54c71b618a
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2014-10-27 15:27:30 -0700

    WIP adding Nimble and Quick

--- ReactiveViewModelTests/RVMTestViewModel.h
+++ ReactiveViewModelTests/RVMTestViewModel.h
@@ -6,7 +6,7 @@
 //  Copyright (c) 2012 GitHub. All rights reserved.
 //
 
-#import "RVMViewModel.h"
+#import <ReactiveViewModel/ReactiveViewModel.h>
 
 @interface RVMTestViewModel : RVMViewModel
 @end
--- ReactiveViewModelTests/RVMViewModelSpec.m
+++ ReactiveViewModelTests/RVMViewModelSpec.m
@@ -6,9 +6,14 @@
 //  Copyright (c) 2012 GitHub. All rights reserved.
 //
 
+#import <Nimble/Nimble.h>
+#import <Quick/Quick.h>
+#import <ReactiveCocoa/ReactiveCocoa.h>
+#import <ReactiveViewModel/ReactiveViewModel.h>
+
 #import "RVMTestViewModel.h"
 
-SpecBegin(RVMViewModel)
+QuickSpecBegin(RVMViewModelSpec)
 
 __block RVMTestViewModel *viewModel;
 
@@ -18,50 +23,50 @@ beforeEach(^{
 
 describe(@"active property", ^{
 	it(@"should default to NO", ^{
-		expect(viewModel.active).to.beFalsy();
+		expect(viewModel.active).to(beFalsy());
 	});
 
 	it(@"should send on didBecomeActiveSignal when set to YES", ^{
 		__block NSUInteger nextEvents = 0;
 		[viewModel.didBecomeActiveSignal subscribeNext:^(RVMViewModel *viewModel) {
-			expect(viewModel).to.beIdenticalTo(viewModel);
-			expect(viewModel.active).to.beTruthy();
+			expect(viewModel).to(beIdenticalTo(viewModel));
+			expect(viewModel.active).to(beTruthy());
 
 			nextEvents++;
 		}];
 
-		expect(nextEvents).to.equal(0);
+		expect(nextEvents).to(equal(0));
 
 		viewModel.active = YES;
-		expect(nextEvents).to.equal(1);
+		expect(nextEvents).to(equal(1));
 
 		// Indistinct changes should not trigger the signal again.
 		viewModel.active = YES;
-		expect(nextEvents).to.equal(1);
+		expect(nextEvents).to(equal(1));
 
 		viewModel.active = NO;
 		viewModel.active = YES;
-		expect(nextEvents).to.equal(2);
+		expect(nextEvents).to(equal(2));
 	});
 
 	it(@"should send on didBecomeInactiveSignal when set to NO", ^{
 		__block NSUInteger nextEvents = 0;
 		[viewModel.didBecomeInactiveSignal subscribeNext:^(RVMViewModel *viewModel) {
-			expect(viewModel).to.beIdenticalTo(viewModel);
-			expect(viewModel.active).to.beFalsy();
+			expect(viewModel).to(beIdenticalTo(viewModel));
+			expect(viewModel.active).to(beFalsy());
 
 			nextEvents++;
 		}];
 
-		expect(nextEvents).to.equal(1);
+		expect(nextEvents).to(equal(1));
 
 		viewModel.active = YES;
 		viewModel.active = NO;
-		expect(nextEvents).to.equal(2);
+		expect(nextEvents).to(equal(2));
 
 		// Indistinct changes should not trigger the signal again.
 		viewModel.active = NO;
-		expect(nextEvents).to.equal(2);
+		expect(nextEvents).to(equal(2));
 	});
 
 	describe(@"signal manipulation", ^{
@@ -91,7 +96,7 @@ describe(@"active property", ^{
 
 		afterEach(^{
 			expect(deallocated).will.beTruthy();
-			expect(completed).to.beTruthy();
+			expect(completed).to(beTruthy());
 		});
 
 		it(@"should forward a signal", ^{
@@ -113,19 +118,19 @@ describe(@"active property", ^{
 					}];
 
 				expectedValues = @[ @1, @2 ];
-				expect(values).to.equal(expectedValues);
-				expect(completed).to.beFalsy();
+				expect(values).to(equal(expectedValues));
+				expect(completed).to(beFalsy());
 
 				viewModel.active = NO;
 
-				expect(values).to.equal(expectedValues);
-				expect(completed).to.beFalsy();
+				expect(values).to(equal(expectedValues));
+				expect(completed).to(beFalsy());
 
 				viewModel.active = YES;
 
 				expectedValues = @[ @1, @2, @1, @2 ];
-				expect(values).to.equal(expectedValues);
-				expect(completed).to.beFalsy();
+				expect(values).to(equal(expectedValues));
+				expect(completed).to(beFalsy());
 			}
 		});
 
@@ -143,14 +148,14 @@ describe(@"active property", ^{
 					}];
 
 				expectedValues = @[ @0 ];
-				expect(values).to.equal(expectedValues);
-				expect(completed).to.beFalsy();
+				expect(values).to(equal(expectedValues));
+				expect(completed).to(beFalsy());
 
 				[subject sendNext:@1];
 
 				expectedValues = @[ @0, @1 ];
-				expect(values).to.equal(expectedValues);
-				expect(completed).to.beFalsy();
+				expect(values).to(equal(expectedValues));
+				expect(completed).to(beFalsy());
 
 				viewModel.active = NO;
 
@@ -158,12 +163,12 @@ describe(@"active property", ^{
 				[subject sendNext:@2];
 				[subject sendNext:@3];
 
-				expect(values).to.equal(expectedValues);
-				expect(completed).to.beFalsy();
+				expect(values).to(equal(expectedValues));
+				expect(completed).to(beFalsy());
 
 				expectedValues = @[ @0, @1, @3 ];
 				expect(values).will.equal(expectedValues);
-				expect(completed).to.beFalsy();
+				expect(completed).to(beFalsy());
 
 				// After reactivating, we should still get this event.
 				[subject sendNext:@4];
@@ -171,22 +176,22 @@ describe(@"active property", ^{
 
 				expectedValues = @[ @0, @1, @3, @4 ];
 				expect(values).will.equal(expectedValues);
-				expect(completed).to.beFalsy();
+				expect(completed).to(beFalsy());
 
 				// And now new events should be instant.
 				[subject sendNext:@5];
 
 				expectedValues = @[ @0, @1, @3, @4, @5 ];
-				expect(values).to.equal(expectedValues);
-				expect(completed).to.beFalsy();
+				expect(values).to(equal(expectedValues));
+				expect(completed).to(beFalsy());
 
 				[subject sendCompleted];
 
-				expect(values).to.equal(expectedValues);
-				expect(completed).to.beTruthy();
+				expect(values).to(equal(expectedValues));
+				expect(completed).to(beTruthy());
 			}
 		});
 	});
 });
 
-SpecEnd
+QuickSpecEnd
