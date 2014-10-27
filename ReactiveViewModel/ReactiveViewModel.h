0bf9ce744b6b6fc8bb0b75859066fa073807a2ed
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2014-10-27 15:20:52 -0700

    Recreate iOS target as a framework

--- ReactiveViewModel/ReactiveViewModel.h
+++ ReactiveViewModel/ReactiveViewModel.h
@@ -2,8 +2,18 @@
 //  ReactiveViewModel.h
 //  ReactiveViewModel
 //
-//  Created by Justin Spahr-Summers on 2013-06-30.
-//  Copyright (c) 2013 GitHub. All rights reserved.
+//  Created by Justin Spahr-Summers on 2014-10-27.
+//  Copyright (c) 2014 GitHub. All rights reserved.
 //
 
-#import "RVMViewModel.h"
+#import <Foundation/Foundation.h>
+
+//! Project version number for ReactiveViewModel.
+FOUNDATION_EXPORT double ReactiveViewModelVersionNumber;
+
+//! Project version string for ReactiveViewModel.
+FOUNDATION_EXPORT const unsigned char ReactiveViewModelVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ReactiveViewModel/PublicHeader.h>
+
+#import <ReactiveViewModel/RVMViewModel.h>
